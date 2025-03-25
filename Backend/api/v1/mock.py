2c7b18b7be310568155dd164c110ddb34b9ca702
Alice <matthew63731417@gmail.com>
2025-03-25 14:56:16 +0800

    Delete some file

--- Backend/api/v1/mock.py
+++ Backend/api/v1/mock.py
@@ -124,6 +124,7 @@ async def get_mock_exams(mock_type: ExamType):
 
     return [] if mock_exam_data is None else mock_exam_data
 
+
 @router.post("/mock/new/exam/")
 async def create_new_exam(exam_prams: CreateNewExamParamsModel) -> ExamsInfoModel:
     """
@@ -304,7 +305,7 @@ async def submit(submitted_exam: SubmittedExamModel) -> Optional[int]:
     logger.debug(submitted_exam)
     submission_id = mysql_client.insert_mock_exam_submitted_question(submitted_exam)
     logger.debug(submission_id)
-    
+
     return submission_id if submission_id else None
 
 
@@ -320,7 +321,7 @@ async def query_mock_exam_results(submission_id: int) -> Optional[ExamResultMode
     logger.debug(submission_id)
     exam_results = mysql_client.query_mock_exam_results(submission_id)
     logger.debug(exam_results)
-    
+
     return exam_results if exam_results else None
 
 
