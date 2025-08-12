ff6ed6219477420cf68d92d40d64811aed46ab8c
TyrantRey <63731417matthew@gmail.com>
2025-08-12 18:17:16 +0800

    Frontend deployment

--- Frontend/build.ps1
+++ Frontend/build.ps1
@@ -1,6 +1,18 @@
 # Build.ps1
 # Code by AkinoAlice@TyrantRey
 
+$envVars = @{}
+Get-Content ".env" | ForEach-Object {
+    if ($_ -match "^\s*#") { return } # skip comments
+    if ($_ -match "^\s*$") { return } # skip empty lines
+    $parts = $_ -split '=', 2
+    $name = $parts[0].Trim()
+    $value = $parts[1].Trim()
+    $envVars[$name] = $value
+}
+
+$NEXT_PUBLIC_API_URL = $envVars["NEXT_PUBLIC_API_URL"]
+
 $remoteUrl = git config --get remote.origin.url
 $repoName = [System.IO.Path]::GetFileNameWithoutExtension($remoteUrl) + "_frontend"
 
@@ -8,9 +20,11 @@ $imageName = $repoName.ToLower().Replace('-', '_')
 $imageTag = Get-Date -Format "yyyyMMdd-HHmmss"
 
 Write-Host "Building image: ${imageName}:$imageTag"
-docker build -t "${imageName}:$imageTag" .
+docker build `
+    --build-arg NEXT_PUBLIC_API_URL=$NEXT_PUBLIC_API_URL `
+    -t "${imageName}:$imageTag" .
 
-if ($LASTEXITCODE -eq 0) {
+if ($LastExitCode -eq 0) {
     Write-Host "Docker image ${imageName}:$imageTag built successfully."
 }
 else {
